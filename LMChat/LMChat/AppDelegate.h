19f07a9e2d71dd0bce5eec243608754a6ea08316
CoderDoctorLee <496347267@qq.com>
2016-06-24 10:02:19 +0800

    Created Project

---
+++ LMChat/LMChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMChat
+//
+//  Created by CoderDoctorLee on 16/6/24.
+//  Copyright © 2016年 CoderDoctorLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMChat/LMChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMChat
+//
+//  Created by CoderDoctorLee on 16/6/24.
+//  Copyright © 2016年 CoderDoctorLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
